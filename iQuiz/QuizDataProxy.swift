2eb4ade3968774cb2668a3b18d854c33875f0eea
Jacob Alewel <jakealewel@ymail.com>
2015-11-03 22:03:57 -0800

    Added proper quiz data

--- iQuiz/QuizDataProxy.swift
+++ iQuiz/QuizDataProxy.swift
@@ -12,8 +12,11 @@ import UIKit
 class QuizDataProxy {
     
     func loadQuizesWithCompletionHandler(completionHandler: (successful: Bool, dtos: [QuizDataDTO]) -> Void) {
+        
         let quizData = [
-            QuizDataDTO(quizName: "Marvel", quizDescription: "Super Heros")
+            QuizDataDTO(quizName: "Mathematics", quizDescription: "Maths r tght 2 lrn"),
+            QuizDataDTO(quizName: "Marvel", quizDescription: "How super is your knowledge on heros?"),
+            QuizDataDTO(quizName: "Science", quizDescription: "Time to do some science!")
         ];
         
         completionHandler(successful: true, dtos: quizData);
